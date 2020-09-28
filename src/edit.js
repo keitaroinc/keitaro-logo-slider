bd8ce6f37fbc74688dfb8c3129bfaad61df8d9a8
Goce Mitevski <goce.mitevski@keitaro.com>
2020-09-29 00:59:48 +0200

    Add block assets

---
+++ src/edit.js
@@ -0,0 +1,77 @@
+import { Fragment } from '@wordpress/element';
+import { RangeControl } from '@wordpress/components';
+import { InspectorControls, MediaUploadCheck, MediaPlaceholder, RichText } from '@wordpress/block-editor';
+
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
+
+const ALLOWED_MEDIA_TYPES = ['image'];
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
+ *
+ * @param {Object} [props]           Properties passed from the editor.
+ * @param {string} [props.className] Class name generated for the block.
+ *
+ * @return {WPElement} Element to render.
+ */
+export default function Edit({ className, attributes, setAttributes }) {
+  return (
+    <Fragment>
+      <InspectorControls>
+        <RangeControl
+          label="Number of slides"
+          value={parseInt(attributes.numberOfSlides)}
+          onChange={(value) => setAttributes({ numberOfSlides: value })}
+          min={1}
+          max={10}
+        />
+      </InspectorControls>
+      {attributes.logos ? (
+        <div className={className}>
+          <RichText
+            tagName="h2"
+            className="title"
+            value={attributes.title}
+            onChange={(content) => setAttributes({ title: content })}
+            placeholder={__('Catchy title goes here...', 'keitaro-logo-slider')}
+          />
+          {attributes.logos.map((item, key) => <img key={key} src={item.url} className="logo" alt={item.alt}></img>)}
+        </div>) : (
+          <div className='img-edit-before'>
+            <MediaUploadCheck>
+              <MediaPlaceholder
+                onSelect={
+                  (el) => {
+                    setAttributes({ logos: el.map(item => ({ url: item.url, alt: item.alt })) });
+                  }
+                }
+                isAppender={true}
+                className={className}
+                icon={`format-gallery`}
+                allowedTypes={ALLOWED_MEDIA_TYPES}
+                multiple={true}
+                labels={{ title: __('Logos', 'keitaro-logo-slider') }}
+              >
+              </MediaPlaceholder>
+            </MediaUploadCheck>
+          </div>
+        )}
+    </Fragment>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,111 @@
+/**
+ * Registers a new block provided a unique name and an object defining its behavior.
+ *
+ * @see https://developer.wordpress.org/block-editor/developers/block-api/#registering-a-block
+ */
+import { registerBlockType } from '@wordpress/blocks';
+
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * All files containing `style` keyword are bundled together. The code used
+ * gets applied both to the front of your site and to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './style.scss';
+
+/**
+ * Internal dependencies
+ */
+import Edit from './edit';
+import save from './save';
+
+/**
+ * Every block starts by registering a new block type definition.
+ *
+ * @see https://developer.wordpress.org/block-editor/developers/block-api/#registering-a-block
+ */
+registerBlockType('keitaro/logo-slider', {
+  /**
+   * This is the display title for your block, which can be translated with `i18n` functions.
+   * The block inserter will show this name.
+   */
+  title: __('Logo Slider', 'keitaro-logo-slider'),
+
+  /**
+   * This is a short description for your block, can be translated with `i18n` functions.
+   * It will be shown in the Block Tab in the Settings Sidebar.
+   */
+  description: __(
+    'Easy to use logo slider for your WordPress website.',
+    'keitaro-logo-slider'
+  ),
+
+  /**
+   * Blocks are grouped into categories to help users browse and discover them.
+   * The categories provided by core are `common`, `embed`, `formatting`, `layout` and `widgets`.
+   */
+  category: 'media',
+
+  /**
+   * An icon property should be specified to make it easier to identify a block.
+   * These can be any of WordPress’ Dashicons, or a custom svg element.
+   */
+  icon: 'image-flip-horizontal',
+
+  /**
+   * Optional block extended support features.
+   */
+  supports: {
+    // Removes support for an HTML mode.
+    align: true,
+  },
+
+  attributes: {
+    title: {
+      type: 'string',
+      source: 'html',
+      selector: '.title',
+      default: __('Trusted by', 'keitaro-logo-slider')
+    },
+    numberOfSlides: {
+      type: 'number',
+      default: 1,
+      minimum: 1
+    },
+    logos: {
+      type: 'array',
+      source: 'query',
+      selector: 'img',
+      query: {
+        url: {
+          type: 'string',
+          source: 'attribute',
+          attribute: 'src',
+        },
+        alt: {
+          type: 'string',
+          source: 'attribute',
+          attribute: 'alt',
+        },
+      }
+    },
+  },
+
+  /**
+   * @see ./edit.js
+   */
+  edit: Edit,
+
+  /**
+   * @see ./save.js
+   */
+  save,
+});
