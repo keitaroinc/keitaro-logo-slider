8c0570e0a9f89312ab49eaaa54f545d1c813773f
Goce Mitevski <goce.mitevski@keitaro.com>
2020-09-29 00:59:11 +0200

    Create Gutenberg block

---
+++ keitaro-logo-slider.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Plugin Name:     Logo Slider
+ * Description:     Easy to use logo slider for your website.
+ * Version:         0.1.0
+ * Author:          Goce Mitevski
+ * License:         GPL-2.0-or-later
+ * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:     keitaro-logo-slider
+ *
+ * @package         keitaro-logo-slider
+ */
+
+/**
+ * Registers all block assets so that they can be enqueued through the block editor
+ * in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/applying-styles-with-stylesheets/
+ */
+function keitaro_logo_slider_block_init() {
+	$dir = dirname( __FILE__ );
+
+	$script_asset_path = "$dir/build/index.asset.php";
+	if ( ! file_exists( $script_asset_path ) ) {
+		throw new Error(
+			'You need to run `npm start` or `npm run build` for the "keitaro/logo-slider" block first.'
+		);
+	}
+	$index_js     = 'build/index.js';
+	$script_asset = require( $script_asset_path );
+	wp_register_script(
+		'keitaro-logo-slider-block-editor',
+		plugins_url( $index_js, __FILE__ ),
+		$script_asset['dependencies'],
+		$script_asset['version']
+	);
+	wp_set_script_translations( 'keitaro-logo-slider-block-editor', 'keitaro-logo-slider' );
+
+	$editor_css = 'build/index.css';
+	wp_register_style(
+		'keitaro-logo-slider-block-editor',
+		plugins_url( $editor_css, __FILE__ ),
+		array(),
+		filemtime( "$dir/$editor_css" )
+	);
+
+	$style_css = 'build/style-index.css';
+	wp_register_style(
+		'keitaro-logo-slider-block',
+		plugins_url( $style_css, __FILE__ ),
+		array(),
+		filemtime( "$dir/$style_css" )
+	);
+
+	register_block_type( 'keitaro/logo-slider', array(
+		'editor_script' => 'keitaro-logo-slider-block-editor',
+		'editor_style'  => 'keitaro-logo-slider-block-editor',
+		'style'         => 'keitaro-logo-slider-block',
+	) );
+}
+add_action( 'init', 'keitaro_logo_slider_block_init' );
