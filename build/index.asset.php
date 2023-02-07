86adb416cfa32c20d6c91a5b1e72579508c63373
Goce Mitevski <goce.mitevski@keitaro.com>
2023-02-07 11:32:09 +0100

    Update index.asset.php

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '0352022f8abc18de37754ca909e555a0');
+<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'eb343642d92fc2f9c8c80611048fce65');
