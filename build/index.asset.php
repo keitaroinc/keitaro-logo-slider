c3d7ea6e845605e067fe4ec6cbba4180e5deb57a
Hristijan Petkovski <hristijan.petkovski@keitaro.com>
2023-02-08 10:07:11 +0100

    Build static assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '54c13a33f9e4a41a6d9784680e77ab57');
+<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '58a22d0105d91e0dc34a5b587b98452f');
