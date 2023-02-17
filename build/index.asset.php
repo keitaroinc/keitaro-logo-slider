59e72e745bcad82c5e65ce1f5e06946439440838
Goce Mitevski <goce.mitevski@keitaro.com>
2023-02-17 20:02:23 +0100

    Rebuild assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4da83f9aceea871dc39af1fc66dacd6f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '826c1c5812dc60d8d658');
